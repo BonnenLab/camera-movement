2c656a105ae29fa982a2fcc6eb0f6e92429aab10
Zachary Petroff <59905848+ZachPetroff@users.noreply.github.com>
2022-08-07 02:06:47 -0400

    Add files via upload

--- models/ngransac/make.sh
+++ models/ngransac/make.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-#SBATCH -J rigidmask0
+#SBATCH -J make
 #SBATCH -p gpu
 #SBATCH -o filename_%j.txt
 #SBATCH -e filename_%j.err
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=zpetroff@iu.edu
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node v100:1
 #SBATCH --time=01:00:00
